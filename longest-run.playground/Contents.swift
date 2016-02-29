df06fe269874adb56e2da5ec82ebb4a25652a82a
Maciej Matyjas <mmatyjas@expedia.com>
2016-02-29 16:46:55 +0000

    in the beginning, there was this commit...

---
+++ longest-run.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+func longestRun(whole: String) -> String {
+    return whole
+}
+
+// identity test
+var zz = "zzzz"
+assert(zz == longestRun(zz), "\(zz) should be the longest run of chars")
+
+// book
+// var book = "book"
+// assert("oo" == longestRun(book), "longest run in \(book) is 'oo'")
+
