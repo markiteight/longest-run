ea4ed71f1d89f5acb90b593cbb069bfe89c1fce5
Mark Stanfield <mark.stanfield@gmail.com>
2016-03-03 11:48:32 +0000

    Uncommented the book test - which is currently failing. Refactored the code to make it simpler to understand.

--- longest-run.playground/Contents.swift
+++ longest-run.playground/Contents.swift
@@ -6,15 +6,21 @@ This excercise lends itself to TDD and we have provided a few basic tests below.
 
 import Cocoa
 
-func longestRun(whole: String) -> String {
-    return whole
+func longestRun(input: String) -> String {
+    return input
 }
 
 // identity test
-var zzzz = "zzzz"
-assert(zzzz == longestRun(zzzz), "\(zzzz) should be the longest run of chars")
+var input = "zzzz"
+var expectedOutput = "zzzz"
+var errorMessage = "\(input) should be the longest run of chars"
+var actualOutput = longestRun(input)
+assert(expectedOutput == actualOutput, errorMessage)
 
-// book
-// var book = "book"
-// assert("oo" == longestRun(book), "longest run in \(book) is 'oo'")
+// book test
+input = "book"
+expectedOutput = "oo"
+errorMessage = "expect that longest run in '\(input)' is '\(expectedOutput)'"
+actualOutput = longestRun(input)
+assert(expectedOutput == actualOutput, errorMessage)
 
