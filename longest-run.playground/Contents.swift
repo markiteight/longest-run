e13f09d38333e5579ea5e0a0552dc8ce3b2f35b8
Mark Stanfield <mark.stanfield@gmail.com>
2016-03-03 12:03:21 +0000

    Created a loop to go through each character and store in a string. Note: this is not yet looking for duplicates.

--- longest-run.playground/Contents.swift
+++ longest-run.playground/Contents.swift
@@ -1,4 +1,4 @@
-/* 
+/*
 The challenge here is to implement the longestRun function so that the
 longest run of the same continuous character will be selected from a String.
 This excercise lends itself to TDD and we have provided a few basic tests below.
@@ -7,15 +7,30 @@ This excercise lends itself to TDD and we have provided a few basic tests below.
 import Cocoa
 
 func longestRun(input: String) -> String {
-    return input
+    // interogate the string by looking @ each character,
+    var output = ""
+    for character in input {
+        character
+        // if a duplicate occurs
+        // store that
+        output = output + "\(character)"
+        //and move on to next character & repeat
+    }
+    
+    return output
 }
 
-// identity test
-var input = "zzzz"
-var expectedOutput = "zzzz"
-var errorMessage = "\(input) should be the longest run of chars"
-var actualOutput = longestRun(input)
-assert(expectedOutput == actualOutput, errorMessage)
+var input = ""
+var expectedOutput = ""
+var errorMessage = ""
+var actualOutput = ""
+
+//// identity test
+//input = "zzzz"
+//expectedOutput = "zzzz"
+//errorMessage = "\(input) should be the longest run of chars"
+//actualOutput = longestRun(input)
+//assert(expectedOutput == actualOutput, errorMessage)
 
 // book test
 input = "book"
@@ -23,4 +38,11 @@ expectedOutput = "oo"
 errorMessage = "expect that longest run in '\(input)' is '\(expectedOutput)'"
 actualOutput = longestRun(input)
 assert(expectedOutput == actualOutput, errorMessage)
+
+//// books test
+//input = "books"
+//expectedOutput = "oo"
+//errorMessage = "expect that longest run in '\(input)' is '\(expectedOutput)'"
+//actualOutput = longestRun(input)
+//assert(expectedOutput == actualOutput, errorMessage)
 
